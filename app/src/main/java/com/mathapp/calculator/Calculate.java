014d22df389654a8d1a5c88e2bf0c21db9617c37
sazgez <wishell.rock.4@gmail.com>
2023-04-06 22:06:33 +0300

    step-3

--- app/src/main/java/com/mathapp/calculator/Calculate.java
+++ app/src/main/java/com/mathapp/calculator/Calculate.java
@@ -1,26 +1,67 @@
 package com.mathapp.calculator;
 
+import java.util.Stack;
+
 public class Calculate {
-    private int firstNo, secondNo;
+    private Stack<Integer> numbers;
+    private Stack<Character> operators;
 
-    public Calculate(int firstNo, int secondNo) {
-        this.firstNo = firstNo;
-        this.secondNo = secondNo;
+    public Calculate() {
+        numbers = new Stack<>();
+        operators = new Stack<>();
     }
 
-    public int add(){
-        return firstNo + secondNo;
+    public int evaluate(String input) {
+        for (int i = 0; i < input.length(); i++) {
+            char c = input.charAt(i);
+            if (Character.isDigit(c)) {
+                int num = c - '0'; // the difference of their Unicode values will give us the integer value. (The Unicode values of the digits '0' through '9' are consecutive.)
+                while (i+1 < input.length() && Character.isDigit(input.charAt(i+1))) { // checking next char whether is a char
+                    num = num * 10 + (input.charAt(i+1) - '0'); // e.g. num = 2 * 10 + (3) => num = 23
+                    i++;
+                }
+                numbers.push(num);
+            } else if (c == '+' || c == '-' || c == '*' || c == '/') {
+                while (!operators.isEmpty() && hasPrecedence(c, operators.peek())) {
+                    performOperation();
+                }
+                operators.push(c);
+            } else if (c == '(') {
+                operators.push(c);
+            } else if (c == ')') {
+                while (operators.peek() != '(') {
+                    performOperation(); // calculate inside the parenthesis
+                }
+                operators.pop(); // remove '(' from stack
+            }
+        }
+        while (!operators.isEmpty()) {
+            performOperation();
+        }
+        return numbers.pop();
     }
 
-    public int sub(){
-        return firstNo - secondNo;
+    private void performOperation() {
+        int num2 = numbers.pop();
+        int num1 = numbers.pop();
+        char op = operators.pop();
+        int result = 0;
+        switch(op) {
+            case '+': result = num1 + num2; break;
+            case '-': result = num1 - num2; break;
+            case '*': result = num1 * num2; break;
+            case '/': result = num1 / num2; break;
+        }
+        numbers.push(result);
     }
 
-    public int mult(){
-        return firstNo * secondNo;
-    }
-
-    public int div(){
-        return firstNo / secondNo;
+    private boolean hasPrecedence(char op1, char op2) {
+        if (op2 == '(' || op2 == ')') {
+            return false;
+        }
+        if ((op1 == '*' || op1 == '/') && (op2 == '+' || op2 == '-')) {
+            return false;
+        }
+        return true;
     }
 }
--- app/src/main/java/com/mathapp/calculator/MainActivity.java
+++ app/src/main/java/com/mathapp/calculator/MainActivity.java
@@ -9,10 +9,129 @@ import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private EditText display;
+    private Calculate calculate;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        display = (EditText) findViewById(R.id.editTextEquation);
+        calculate = new Calculate();
+
+        // trial
+        String input = "23+1*3-(1-(4/2))";
+        Calculate calculate = new Calculate();
+        System.out.println(calculate.evaluate(input));
+    }
+
+    public void buttonClick(View view) {
+        int result;
+        String input = display.getText().toString();
+
+        switch (view.getId()) {
+            case R.id.button0:
+                input = input.concat("0");
+                display.setText(input);
+                break;
+            case R.id.button1:
+                input = input.concat("1");
+                display.setText(input);
+                break;
+            case R.id.button2:
+                input = input.concat("2");
+                display.setText(input);
+                break;
+            case R.id.button3:
+                input = input.concat("3");
+                display.setText(input);
+                break;
+            case R.id.button4:
+                input = input.concat("4");
+                display.setText(input);
+                break;
+            case R.id.button5:
+                input = input.concat("5");
+                display.setText(input);
+                break;
+            case R.id.button6:
+                input = input.concat("6");
+                display.setText(input);
+                break;
+            case R.id.button7:
+                input = input.concat("7");
+                display.setText(input);
+                break;
+            case R.id.button8:
+                input = input.concat("8");
+                display.setText(input);
+                break;
+            case R.id.button9:
+                input = input.concat("9");
+                display.setText(input);
+                break;
+            case R.id.buttonAddition:
+                input = input.concat("+");
+                display.setText(input);
+                break;
+            case R.id.buttonSubtraction:
+                input = input.concat("-");
+                display.setText(input);
+                break;
+            case R.id.buttonMultiplication:
+                input = input.concat("*");
+                display.setText(input);
+                break;
+            case R.id.buttonDivision:
+                input = input.concat("/");
+                display.setText(input);
+                break;
+            case R.id.buttonFactorial:
+                input = input.concat("!");
+                display.setText(input);
+                break;
+            case R.id.buttonExponent:
+                input = input.concat("^");
+                display.setText(input);
+                break;
+            case R.id.buttonPercentage:
+                input = input.concat("%(");
+                display.setText(input);
+                break;
+            case R.id.buttonSquareRoot:
+                input = input.concat("√(");
+                display.setText(input);
+                break;
+            case R.id.buttonDot:
+                input = input.concat(".");
+                display.setText(input);
+                break;
+            case R.id.buttonOpenParenthesis:
+                input = input.concat("(");
+                display.setText(input);
+                break;
+            case R.id.buttonCloseParenthesis:
+                input = input.concat(")");
+                display.setText(input);
+                break;
+            case R.id.buttonDelete:
+                if(input.length() != 0) {
+                    if(input.length() == 1)
+                        input = "";
+                    else
+                        input = input.substring(0, input.length()-1); // substring(inclusive, exclusive)
+                }
+                display.setText(input);
+                break;
+            case R.id.buttonClear:
+                input = "";
+                display.setText(input);
+                break;
+            case R.id.buttonEqual:
+                result = calculate.evaluate(input);
+                display.setText(Integer.toString(result));
+                break;
+        }
     }
 }
